fa97543f24ab9124cbeb63f866268c7cac5dc32e
varomix Wey <varomix@gmail.com>
2016-03-16 08:37:38 -0600

    added old opengl stuff back, seems like it works for both versions, kinda like glew

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"SparkyEngineGO/gfx"
-	"fmt"
 	"runtime"
 
-	"github.com/go-gl/gl/v3.3-core/gl"
+	"github.com/go-gl/gl/v2.1/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 )
 
@@ -25,7 +24,7 @@ func main() {
 
 	gl.ClearColor(0.2, 0.3, 0.8, 1.0)
 	gl.ClearDepth(1)
-	fmt.Printf("version_: %d", *gl.GetString(gl.VERSION))
+	// fmt.Printf("version_: %d", *gl.GetString(gl.VERSION))
 
 	var vao uint32
 	gl.GenVertexArrays(1, &vao)
@@ -36,6 +35,13 @@ func main() {
 		gfx.Clear()
 
 		gl.DrawArrays(gl.BUFFER, 0, 6)
+		// old school gl drawing for testing
+		gl.Begin(gl.QUADS)
+		gl.Vertex2f(-0.5, 0.5)
+		gl.Vertex2f(0.5, 0.5)
+		gl.Vertex2f(0.5, -0.5)
+		gl.Vertex2f(-0.5, -0.5)
+		gl.End()
 
 		gfx.Update(window)
 	}
