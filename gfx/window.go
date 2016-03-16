62d92951b8e9ac8ce47235f52382111fbc9c0ed5
varomix Wey <varomix@gmail.com>
2016-03-16 00:26:40 -0600

    switched to opengl 3.3

--- gfx/window.go
+++ gfx/window.go
@@ -1,7 +1,7 @@
 package gfx
 
 import (
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 )
 
--- main.go
+++ main.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"SparkyEngineGO/gfx"
+	"fmt"
 	"runtime"
 
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/go-gl/gl/v3.3-core/gl"
 	"github.com/go-gl/glfw/v3.1/glfw"
 )
 
@@ -24,21 +25,18 @@ func main() {
 
 	gl.ClearColor(0.2, 0.3, 0.8, 1.0)
 	gl.ClearDepth(1)
+	fmt.Printf("version_: %d", *gl.GetString(gl.VERSION))
+
+	var vao uint32
+	gl.GenVertexArrays(1, &vao)
+	gl.BindVertexArray(vao)
 
 	for !window.ShouldClose() {
 		// DO OpenGL Stuff
 		gfx.Clear()
 
-		// old school gl drawing for testing
-		gl.Begin(gl.QUADS)
-		gl.Vertex2f(-0.5, 0.5)
-		gl.Vertex2f(0.5, 0.5)
-		gl.Vertex2f(0.5, -0.5)
-		gl.Vertex2f(-0.5, -0.5)
-		gl.End()
+		gl.DrawArrays(gl.BUFFER, 0, 6)
 
-		// window.SwapBuffers()
-		// glfw.PollEvents()
 		gfx.Update(window)
 	}
 
